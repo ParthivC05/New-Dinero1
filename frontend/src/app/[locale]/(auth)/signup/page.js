85b3055a9aef0461e594a72d46c482839f1812e1
Raman Prasad <ramanprasad.0203@gmail.com>
2025-09-17 11:37:04 +0530

    refactor: created the layout

---
+++ frontend/src/app/[locale]/(auth)/signup/page.js
@@ -0,0 +1,14 @@
+import Signup from '@/components/LoginSignup/components/signup';
+
+export default function Page() {
+  return <Signup />;
+}
+
+
+
+//apple /banana/egg(boiled)/pomegranate/pineApple
+//sb khati ho/ rice quantity (less)
+//saam ko kuch khana jarurui  nhi h
+//sb khati ho/ rice quantity (less)
+//sugar (no sugar)
+//walk
