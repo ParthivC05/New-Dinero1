c93675d1050e0cfb755c4d6d4f98556d6652aacc
raman-pervadorit <rewati@pervadorit.com>
2025-09-18 01:23:38 +0530

    style: changed letter with logo

--- frontend/src/components/LoginSignup/components/googleSignupButton.js
+++ frontend/src/components/LoginSignup/components/googleSignupButton.js
@@ -1,16 +1,33 @@
 import React from 'react';
 import LineWithText from './lineWithText';
+import Image from 'next/image';
 
 function GoogleFacebookSignupButton() {
   return (
     <div className="max-w-xl mx-auto">
       <LineWithText title="Or Register with" />
       <div className="flex gap-8 pt-4 justify-center items-center">
-        <button className="bg-slate-100 px-4 py-2 rounded ">
-          <span className="text-red-500 font-bold">G</span> Google
+        <button className="bg-slate-100 flex justify-center items-center hover:bg-gray-100 transition-colors  px-4 py-2 rounded ">
+          <Image
+            src="https://img.icons8.com/color/24/000000/google-logo.png"
+            className="w-5 h-5 mr-3"
+            width={5}
+            height={5}
+            alt="Google logo"
+            aria-hidden="true"
+          />
+          <span>Google</span>
         </button>
-        <button className="bg-slate-100 px-4 py-2 rounded ">
-          <span className="text-blue-500 font-bold">f</span> Facebook
+        <button className="bg-slate-100 flex justify-center items-center hover:bg-gray-100 transition-colors  px-4 py-2 rounded">
+          <Image
+            src="https://img.icons8.com/color/24/000000/facebook-new.png"
+            className="w-5 h-5 mr-3"
+            width={5}
+            height={5}
+            alt="Facebook logo"
+            aria-hidden="true"
+          />
+          <span>Facebook</span>
         </button>
       </div>
     </div>
