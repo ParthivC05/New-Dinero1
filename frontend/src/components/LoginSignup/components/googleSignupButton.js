3e4e165e750260f3ad5916da57721bc79665a1a1
raman-pervadorit <rewati@pervadorit.com>
2025-09-18 16:57:13 +0530

    feat: integrate api with googleFacebookSignUpButton

--- frontend/src/components/LoginSignup/components/googleSignupButton.js
+++ frontend/src/components/LoginSignup/components/googleSignupButton.js
@@ -7,7 +7,13 @@ function GoogleFacebookSignupButton() {
     <div className="max-w-xl mx-auto">
       <LineWithText title="Or Register with" />
       <div className="flex gap-8 pt-4 justify-center items-center">
-        <button className="bg-slate-100 flex justify-center items-center hover:bg-gray-100 transition-colors  px-4 py-2 rounded ">
+        <button
+          className="bg-slate-100 flex justify-center items-center hover:bg-gray-100 transition-colors  px-4 py-2 rounded "
+          onClick={() => {
+            window.location.href =
+              process.env.NEXT_PUBLIC_BACKEND_URL + '/api/v1/auth/sso/google';
+          }}
+        >
           <Image
             src="https://img.icons8.com/color/24/000000/google-logo.png"
             className="w-5 h-5 mr-3"
@@ -18,7 +24,13 @@ function GoogleFacebookSignupButton() {
           />
           <span>Google</span>
         </button>
-        <button className="bg-slate-100 flex justify-center items-center hover:bg-gray-100 transition-colors  px-4 py-2 rounded">
+        <button
+          onClick={() => {
+            window.location.href =
+              process.env.NEXT_PUBLIC_BACKEND_URL + '/api/v1/auth/sso/facebook';
+          }}
+          className="bg-slate-100 flex justify-center items-center hover:bg-gray-100 transition-colors  px-4 py-2 rounded"
+        >
           <Image
             src="https://img.icons8.com/color/24/000000/facebook-new.png"
             className="w-5 h-5 mr-3"
