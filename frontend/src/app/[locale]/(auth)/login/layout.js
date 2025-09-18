cd8cde282ad5bb0cdede4a27fb5d5ebf2137dc21
raman-pervadorit <rewati@pervadorit.com>
2025-09-18 14:35:02 +0530

    feat:added toaster in layout

--- frontend/src/app/[locale]/(auth)/login/layout.js
+++ frontend/src/app/[locale]/(auth)/login/layout.js
@@ -1,13 +1,16 @@
 import { StateProvider } from '@/store';
+import { Toaster } from '@/components/ui/toaster';
+
 import './../../global.scss';
-export default function RootLayout({ children}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className="">
-        <StateProvider>{children}</StateProvider>
+        <StateProvider>
+          {children}
+          <Toaster />
+        </StateProvider>
       </body>
     </html>
   );
 }
-
-
